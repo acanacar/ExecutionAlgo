6f701e26d59e4c2d6cd7d785c6466964f9746d9d
acanacar <ahmetcanacar.1@gmail.com>
2020-03-05 15:34:13 +0300

    some dates are generated and new parent orders are added

--- simulation/main.py
+++ simulation/main.py
@@ -1,13 +1,43 @@
 from simulation.TWAP import *
 
+
+date1 = '2017-01-03'
+date2 = '2017-01-04'
+
+x = pd.date_range(date1, date2, freq='1s')
+x = x[x.indexer_between_time('10:00', '11:00')]
+
+
 twap_1 = TWAP(symbol='AKBNK',
-              parent_order_quantity=8000,
-              start_time=pd.to_datetime('2017-01-03 10:00'),
-              end_time=pd.to_datetime('2017-01-03 13:00'),
+              parent_order_quantity=800,
+              start_time=pd.to_datetime('2017-01-03 10:10'),
+              end_time=pd.to_datetime('2017-01-03 10:30'),
               side='B',
-              one_slice_interval_as_minutes=10)
+              one_slice_interval_as_minutes=.5)
+
+twap_2 = TWAP(symbol='AKBNK',
+              parent_order_quantity=1000,
+              start_time=pd.to_datetime('2017-01-03 10:03'),
+              end_time=pd.to_datetime('2017-01-03 10:20'),
+              side='B',
+              one_slice_interval_as_minutes=.5)
 
 twap_1.create_child_orders()
+twap_2.create_child_orders()
+
+parent_orders = [twap_1,twap_2]
 for i in twap_1.child_orders:
-    print(i)
-print('x')
+    print(i.__dict__)
+
+
+
+for time in x:
+    for parent_order in parent_orders:
+        if parent_order.start_time == time:
+            parent_order.status = 'working'
+        for child_order in parent_order.child_orders_islem:
+            if child_order.order_time == time:
+                child_order.send_order()
+                child_order.status = 'working'
+
+                print(time)
