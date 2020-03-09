f3aee7ed56a3afc5dc17206262c64534447258aa
acanacar <ahmetcanacar.1@gmail.com>
2020-03-09 17:38:56 +0300

    main py is modified

--- simulation/main.py
+++ simulation/main.py
@@ -1,12 +1,10 @@
 from simulation.TWAP import *
 
-
 date1 = '2017-01-03'
 date2 = '2017-01-04'
 
-x = pd.date_range(date1, date2, freq='1s')
-x = x[x.indexer_between_time('10:00', '11:00')]
-
+times = pd.date_range(date1, date2, freq='1s')
+times = times[times.indexer_between_time('10:00', '11:00')]
 
 twap_1 = TWAP(security_id='AKBNK',
               parent_order_quantity=800,
@@ -22,21 +20,36 @@ twap_2 = TWAP(security_id='AKBNK',
               side='B',
               one_slice_interval_as_minutes=.5)
 
-twap_1.create_child_orders()
-twap_2.create_child_orders()
+parent_orders = [twap_1, twap_2]
+for i, parent_order in enumerate(parent_orders):
+    parent_order.parent_code = 'parent_{}'.format(i)
+    parent_order.user_id = 'user_{}'.format(i)
+    parent_order.create_child_orders()
 
-parent_orders = [twap_1,twap_2]
 for i in twap_1.child_orders:
     print(i.__dict__)
 
-
-
-for time in x:
+for time in times:
     for parent_order in parent_orders:
         if parent_order.start_time == time:
-            parent_order.status = 'working'
+            parent_order.status = 3
         for child_order in parent_order.child_orders_islem:
             if child_order.order_time == time:
                 child_order.activate_order()
-                child_order.send_order()
+                res = child_order.send_order()
+                if res is 'success':
+                    parent_order.undisclosed_quantity -= child_order.order_quantity
+                    parent_order.working_quantity += child_order.order_quantity
+                else:
+                    print('ORDER GONDERILEMEDI')
+                res, amount = child_order.get_trade_status()
+                if res is 'full-filled':
+                    child_order.status = 7
+                    parent_order.executed_quantity += child_order.order_quantity
+                    parent_order.working_quantity -= child_order.order_quantity
+                elif res is 'partial-fill':
+                    parent_order.executed_quantity += amount
+                    parent_order.working_quantity -= amount
+                else:
+                    print('CHILD ORDER FILL OLAMADI')
                 print(time)
