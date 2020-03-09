89a181e4b41e8a4fae50043cda71758109cd822c
acanacar <ahmetcanacar.1@gmail.com>
2020-03-09 17:38:34 +0300

    TWAP classes are modified

--- simulation/TWAP.py
+++ simulation/TWAP.py
@@ -6,11 +6,11 @@ class TWAP(object):
 
     def __init__(self, start_time, end_time,
                  security_id,
-                 user_id,
                  parent_order_quantity,
                  side,
                  one_slice_interval_as_minutes,
-                 left_over_action='leave',
+                 user_id = 'user_default',
+                 left_over_action=3,  # 1- Leave 2- Merge 3- Market 4- Payup
                  ):
         self.security_id = security_id
         self.user_id = user_id
@@ -26,8 +26,30 @@ class TWAP(object):
         self.set_interval_count()
         self.set_child_order_quantity()
         self.child_orders = []
-        self.status = 'waiting'
+        self.status = 1
+        # 1- Waiting
+        # 2- Pending Trigger
+        # 3- Working
+        # 4- Hold
+        # 5- Paused
+        # 6- Deleting
+        # 7- Filled
+        # 8- Available
+        # 9- Owned
+        # 10- Pulling
+        # 11- Unmanaged
+        # 12- Initializing
+        # 13- Recovering
         self.parent_code = 'sss'
+        self.executed_quantity  = 0
+        self.working_quantity = 0
+        self.undisclosed_quantity = parent_order_quantity
+        self.algo_name = 'twap'
+        self.algo_stage = 1  # 1 - running 2 - paused 3 - deleting
+
+    def modify_quantity(self, new_quantity):
+        if self.algo_stage == 1:  # running
+            self.parent_order_quantity = new_quantity
 
     def set_twap_time_horizon(self):
         self.twap_time_horizon_as_seconds = int((self.end_time - self.start_time).seconds)
@@ -40,6 +62,9 @@ class TWAP(object):
         self.child_order_quantity = self.parent_order_quantity / self.interval_count
 
     def get_order_submit_times(self):
+        '''
+        :return: child orderslara ait order time lar.
+        '''
         last_child_order_submit_time = self.start_time + \
                                        pd.DateOffset(seconds=
                                                      self.interval_count * self.one_slice_interval_as_seconds)
@@ -60,13 +85,13 @@ class TWAP(object):
 
         order_quantities = self.get_order_quantities()
         self.child_orders = [TWAP_CHILD(order_time=order_submit_time,
-                                        security_id = self.security_id,
+                                        security_id=self.security_id,
                                         user_id=self.user_id,
                                         side=self.side,
                                         order_quantity=order_quantity,
                                         sliced_no=order_idx,
                                         parent_code=self.parent_code,
-                                        status='waiting')
+                                        status=1)
                              for order_idx, (order_submit_time, order_quantity) in
                              enumerate(zip(order_submit_times, order_quantities), start=1)]
         self.child_orders_islem = self.child_orders.copy()
--- simulation/TWAP_CHILD.py
+++ simulation/TWAP_CHILD.py
@@ -1,7 +1,10 @@
 import datetime
 import time
+
+
 class TWAP_CHILD(object):
-    def __init__(self, order_time,security_id,user_id, side,order_quantity, sliced_no, parent_code, status):
+    def __init__(self, order_time, security_id, user_id, side,
+                 order_quantity, sliced_no, parent_code, status):
         self.order_time = order_time
         self.side = side
         self.security_id = security_id
@@ -11,18 +14,22 @@ class TWAP_CHILD(object):
         self.parent_code = parent_code
         self.status = status
         self.order_quantity_done = 0
-        self.order_type = 'limit'
+        self.order_type = 1  # 1- Market 2- Limit 3- Market Limit Market(MLM)
+        # 4- Market To Limit(MTL) 5- Best Limit(BL) 6- Limit To Market (LTM)
 
     def activate_order(self):
-        self.status = 'working'
+        self.status = 3
 
     def send_order(self):
         ts = time.time()
         order_stamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        pass
+        return 'success'
 
     def modify_order(self):
         pass
 
     def cancel_order(self):
         pass
+
+    def modify_quantity(self, new_quantity):
+        self.order_quantity = new_quantity
