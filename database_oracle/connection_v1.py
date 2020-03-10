9f4c322f7a376452163f4c2a0d160cfd9685ec66
acanacar <ahmetcanacar.1@gmail.com>
2020-03-10 17:56:16 +0300

    database_oracle is added

---
+++ database_oracle/connection_v1.py
@@ -0,0 +1,13 @@
+import cx_Oracle
+
+username = 'INTERNET'
+password = 'M'
+ip = '172.30.0.10'
+port = 1521
+encoding = 'UTF-8'
+sid = 'YKYDEV'
+
+con = cx_Oracle.connect('{}/{}@{}:{}/{}'.format(username, password, ip, port, sid))
+ver = con.version.split(".")
+
+con.close()
