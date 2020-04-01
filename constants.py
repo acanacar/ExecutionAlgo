187030573ac7e42dd76ce369ed880961c351cafa
acanacar <ahmetcanacar.1@gmail.com>
2020-04-01 23:14:21 +0300

    database.ini is created

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 project_path = Path('/root/PycharmProjects/ExecutionAlgo')
 websocket_path = project_path / Path('websocket')
 oracle_db_path = project_path / Path('oracle_db')
