09868ce1d86389ac6e6b0e70700c63f9de1d1ad6
acanacar <ahmetcanacar.1@gmail.com>
2020-04-11 15:25:13 +0300

    msg['datetime_pd'] is added for fxplus message

--- websocket/connection_api.py
+++ websocket/connection_api.py
@@ -24,8 +24,10 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 def create_fxplux_response(msg):
     global MSG_COUNT
     if '_i' in msg.keys():
-        # msg['my_date'] = pd.to_datetime(time.time(), unit='s')
         msg['my_time'] = current_milli_time()
+
+        msg['datetime_pd'] = str(pd.to_datetime(msg['my_time'], unit='ms', utc=True)
+                                 .tz_convert('Europe/Istanbul'))
         MSG_COUNT += 1
         if MSG_COUNT % 10 == 0:
             print(f'{MSG_COUNT}. mesaj alindi. :{msg}')
@@ -54,3 +56,125 @@ async def get_async_fxplus(*, insert_function):
                     await insert_function(response)
                 else:
                     print('response is None')
+
+
+async def recv_data(ws, insert_function):
+    while True:
+        message_str = await ws.recv()
+        message = json.loads(message_str)
+        response = create_fxplux_response(message)
+
+        if response is not None:
+            try:
+                await insert_function(response)
+            except Exception as e:
+                print('Insert Error')
+        else:
+            print('response is None')
+
+
+len_o = 0
+
+
+async def display_(conn_pool, query):
+    #     query = f'''SELECT * FROM time_series_5
+    #             WHERE _i='o850'
+    #             AND my_time > {1585791135038}
+    #             ORDER BY datetime DESC
+    # --             LIMIT 2
+    #             '''
+    queries = [
+        f'''
+            SELECT 	*
+            FROM 	timescale_executionalgo_t2
+            WHERE 	1=1
+            AND     _i = 'H2796'
+            AND     last IS NOT NULL
+            AND 	datetime_pd > NOW() - interval '1 MINUTES';'''
+        ,
+        f'''
+            SELECT 	*
+            FROM 	timescale_executionalgo_t2
+            WHERE 	1=1
+            AND     _i = 'H2796'
+            AND     last IS NOT NULL
+            AND 	datetime_pd > NOW() - interval '65 MINUTES';'''
+    ]
+    tasks = [
+        conn_pool.fetch(queries[0]),
+        conn_pool.fetch(queries[1])
+    ]
+    results = asyncio.gather(*tasks)
+
+    while True:
+        global len_o
+
+        """        
+        query = f'''SELECT 	* 
+                    FROM 	timescale_executionalgo_t2
+                    WHERE 	1=1
+                    AND     _i = 'H2796'
+                    AND     last IS NOT NULL 
+                    AND 	datetime_pd > NOW() - interval '1 MINUTES';'''
+        try:
+            output = await conn_pool.fetch(query)
+            if len(output) > 1:
+                print(len(output))
+                len_o = len(output)
+        except Exception as e:
+            print(str(e))
+        """
+        try:
+            output = await results
+            for x in output:
+                if len(x) > 5:
+                    print(len(x))
+        except Exception as e:
+            print(str(e))
+
+
+a = 0
+b = 0
+
+
+async def display__(conn_pool,ind, query):
+    global a, b
+    while 1:
+        try:
+            output = await conn_pool.fetch(query)
+            if len(output) > 5:
+                print(len(output))
+        except Exception as e:
+            print(str(e))
+
+
+QUERIES = [
+    (1, f'''
+            SELECT 	* 
+            FROM 	timescale_executionalgo_t2
+            WHERE 	1=1
+            AND     _i = 'o850'
+            AND     last IS NOT NULL
+            AND 	datetime_pd > NOW() - interval '1 MINUTES';''')
+    ,
+    (2, f'''
+            SELECT 	* 
+            FROM 	timescale_executionalgo_t2
+            WHERE 	1=1
+            AND     _i = 'o850'
+            AND     last IS NOT NULL 
+            AND 	datetime_pd > NOW() - interval '10 MINUTES';''')
+]
+
+
+async def handle_fxplus(insert_function, pool):
+    ws = await websockets.connect(uri='wss://websocket.foreks.com/websocket', ping_interval=14)
+    if await myLogin(websocket=ws):
+        await ws.send(MESSAGES['SUBSCRIBE_MESSAGE'])
+        asyncio.ensure_future(recv_data(ws=ws, insert_function=insert_function))
+        # asyncio.ensure_future(display_(conn_pool=pool))
+        queries = QUERIES
+        for i, query in queries:
+            asyncio.ensure_future(display__(conn_pool=pool,ind=i, query=query))
+
+# 1854157108
